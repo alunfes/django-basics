2525ef05d153b246b139d69c2c947ed9758c94ee
alunfes <ts4792@gmail.com>
2018-12-12 23:22:56 +0900

    db added

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ProductList(models.Model):
+    product_name = models.CharField(max_length=200)
+    product_price = models.FloatField()
+    product_id = models.DateTimeField(auto_now=True)

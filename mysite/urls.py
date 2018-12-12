bf8ada7f4332e31435a5dbc81101fa9a3dccdee5
alunfes <ts4792@gmail.com>
2018-12-12 21:03:53 +0900

    urls, product id based page

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name  ='mysite'
 urlpatterns = [
     path('', views.index, name='index'),
     path('productlist/<int:product_id>', views.productlist, name='productlist'),
